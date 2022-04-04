091f0f457ff395538257cb5270538857a247d70f
natmacira <natimciraolo@gmail.com>
2022-04-04 10:35:57 -0300

    actualización a versión 1.0.3

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Site Version: 1.0.2
+ * Site Version: 1.0.3
  * Site author: Natalia Ciraolo
  * Author website: https://github.com/Natmacira
  * Author e-mail: natimciraolo@gmail.com
  */
 
-define('SITE_VERSION', '1.0.2');
+define('SITE_VERSION', '1.0.3');
 ?>
 <!DOCTYPE html>
 <html lang="en">
